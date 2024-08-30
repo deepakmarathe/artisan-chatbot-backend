c1f00b45d45be7be02f8e3f51d3cc6402c9d19d4
adla_dmarathe <actions@github.com>
2024-08-30 12:06:29 +0530

    update imports for crud

--- chatbot/crud.py
+++ chatbot/crud.py
@@ -1,19 +1,20 @@
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 
-import models
-import schemas
+from .models import User, Message
+from .schemas import UserCreate, MessageCreate, MessageUpdate
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def get_user_by_username(db: Session, username: str):
-    return db.query(models.User).filter(models.User.username == username).first()
+    return db.query(User).filter(User.username == username).first()
 
 
-def create_user(db: Session, user: schemas.UserCreate):
+def create_user(db: Session, user: UserCreate):
     hashed_password = pwd_context.hash(user.password)
-    db_user = models.User(username=user.username, hashed_password=hashed_password)
+    db_user = User(username=user.username, hashed_password=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
@@ -29,8 +30,8 @@ def authenticate_user(db: Session, username: str, password: str):
     return user
 
 
-def create_message(db: Session, message: schemas.MessageCreate, user_id: int):
-    db_message = models.Message(**message.dict(), user_id=user_id)
+def create_message(db: Session, message: MessageCreate, user_id: int):
+    db_message = Message(**message.dict(), user_id=user_id)
     db.add(db_message)
     db.commit()
     db.refresh(db_message)
@@ -38,14 +39,14 @@ def create_message(db: Session, message: schemas.MessageCreate, user_id: int):
 
 
 def get_messages(db: Session, user_id: int, skip: int = 0, limit: int = 10):
-    return db.query(models.Message).filter(models.Message.user_id == user_id).offset(skip).limit(limit)
+    return db.query(Message).filter(Message.user_id == user_id).offset(skip).limit(limit)
 
 
 def get_message(db: Session, message_id: int):
-    return db.query(models.Message).filter(models.Message.id == message_id).first()
+    return db.query(Message).filter(Message.id == message_id).first()
 
 
-def update_message(db: Session, message: schemas.MessageUpdate, message_id: int):
+def update_message(db: Session, message: MessageUpdate, message_id: int):
     db_message = get_message(db, message_id)
     if db_message:
         db_message.content = message.content
