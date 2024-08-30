f54e036af6e68200e0531d8e807818b1bbe76589
adla_dmarathe <actions@github.com>
2024-08-30 13:54:31 +0530

    checkin tests

--- api_curl/user/login.sh
+++ api_curl/user/login.sh
@@ -1,1 +1,3 @@
 curl -X POST "http://localhost:8003/token" -H "Content-Type: application/x-www-form-urlencoded" -d "username=admin&password=admin"
+
+curl -X POST "http://54.157.15.102:8000/token" -H "Content-Type: application/x-www-form-urlencoded" -d "username=admin&password=admin"
