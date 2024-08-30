020defb12e460dc711ca79f3bacc16e0b7cd2c2d
adla_dmarathe <actions@github.com>
2024-08-30 13:16:20 +0530

    server run script

--- run_server.sh
+++ run_server.sh
@@ -1,12 +1,13 @@
 #uvicorn main:app --reload --host 0.0.0.0 --port 8002 --log-level debug
 
-pip3.9 install -r requirements.txt
+#pip3.9 install -r requirements.txt
+#sudo python3.9 -m pip install -r requirements.txt
 
 # Replace <port_number> with the actual port number you want to check
-PORT=8003
+PORT=8000
 # Find the process ID using the specified port
 lsof -i :$PORT -t | xargs kill -9
 
-python3.9 chatbot/main.py
+#python3.9 chatbot/main.py
 
-uvicorn chatbot.main:app --reload --port 8003 --host 0.0.0.0 --log-level debug
+uvicorn chatbot.main:app --reload --port $PORT --host 0.0.0.0 --log-level debug
